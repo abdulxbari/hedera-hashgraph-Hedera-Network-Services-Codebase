8f9f8d16c3bb3e3b96784c852675fd6f390d0d26
Michael Tinker <michael.tinker@swirldslabs.com>
2022-09-20 15:58:35 -0500

    Finish javadoc on basic types
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/src/main/java/com/hedera/services/ledger/v2/TransactionalKeyValueStore.java
+++ hedera-node/src/main/java/com/hedera/services/ledger/v2/TransactionalKeyValueStore.java
@@ -1,13 +1,15 @@
 package com.hedera.services.ledger.v2;
 
 import com.hedera.services.ledger.v2.frame.ReversibleChange;
+import com.hedera.services.ledger.v2.keys.HederaKey;
 
 import java.util.function.Consumer;
 
 /**
  * A {@link MutableKeyValueStore} that permits buffering a set of key-value
  * mappings into a "frame" before either committing or reverting them all
- * atomically.
+ * atomically. It also supports reviewing all changes from a specified list
+ * of key types.
  *
  * <p><b>IMPORTANT:</b> it is possible to call {@code beginFrame()} several
  * times, creating a "stack" of frames, where all changes are buffered in the
@@ -15,11 +17,35 @@ import java.util.function.Consumer;
  * start buffering in the new topmost frame.
  */
 public interface TransactionalKeyValueStore extends MutableKeyValueStore {
+    /**
+     * Begins a new frame of changes that should be committed or reverted atomically.
+     */
     void beginFrame();
 
+    /**
+     * Reverts all changes accumulated since the last call to {@code beginFrame()}.
+     *
+     * @throws IllegalStateException if no frame has begun
+     */
     void revertFrame();
 
+    /**
+     * Commits all changes accumulated since the last call to {@code beginFrame()}.
+     * If the current frame is the only frame, a "commit" means persisting new key/value
+     * mappings to the store represented by this object. Otherwise, a "commit" simply
+     * squashes the change sin the current frame to the frame below it.
+     *
+     * @throws IllegalStateException if no frame has begun
+     */
     void commitFrame();
 
-    void reviewChangesInFrame(Consumer<ReversibleChange> observer);
+    /**
+     * Exposes all pending changes of the requested key types in the current frame to the given observer.
+     *
+     * <p><b>IMPORTANT:</b> The observer will be called at most once for each distinct key.
+     *
+     * @param observer a callback to receive information about pending changes
+     * @param keyTypes the key types of interest
+     */
+    void reviewChangesInFrame(Consumer<ReversibleChange<?>> observer, Class<? extends HederaKey>... keyTypes);
 }
--- hedera-node/src/main/java/com/hedera/services/ledger/v2/frame/ReversibleChange.java
+++ hedera-node/src/main/java/com/hedera/services/ledger/v2/frame/ReversibleChange.java
@@ -1,4 +1,45 @@
 package com.hedera.services.ledger.v2.frame;
 
-public interface ReversibleChange {
+import com.hedera.services.ledger.v2.keys.HederaKey;
+
+public interface ReversibleChange<V> {
+    HederaKey key();
+    V oldValue();
+    V newValue();
+
+    default boolean isIntChange() {
+        return false;
+    }
+
+    default boolean isLongChange() {
+        return false;
+    }
+
+    default boolean isBooleanChange() {
+        return false;
+    }
+
+    default int oldIntValue() {
+        throw new UnsupportedOperationException();
+    }
+
+    default int newIntValue() {
+        throw new UnsupportedOperationException();
+    }
+
+    default long oldLongValue() {
+        throw new UnsupportedOperationException();
+    }
+
+    default long newLongValue() {
+        throw new UnsupportedOperationException();
+    }
+
+    default boolean oldBooleanValue() {
+        throw new UnsupportedOperationException();
+    }
+
+    default boolean newBooleanValue() {
+        throw new UnsupportedOperationException();
+    }
 }
